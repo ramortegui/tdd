26dfee9177f82326abe9c05e3a9a0e3a9764c6ec
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-08-19 17:11:03 -0400

    Add verify method, with diagnosticgs

--- calculator.rb
+++ calculator.rb
@@ -2,8 +2,25 @@ class Calculator
     def add ( x , y )
       x + y
     end
+    def substract ( x, y )
+      x - y 
+    end
+end
+
+def verify ( expected, actual, message )
+  if actual == expected 
+    print "\033[32m#{message} passed\033[0m\n"
+  else
+    print "Expected: #{expected} but got: #{actual} - #{message} \n"
+    print "\e[31m#{message} failed.\e[0m\n"
+  end
 end
 
 calculator = Calculator.new
 result = calculator.add( 1, 1 )
-puts result
+verify( 2, result, 'Addition' )
+result2 = calculator.substract(2,1)
+verify( 2, result2, 'Substraction' )
+
+
+
