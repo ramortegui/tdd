e966dadadf0584594766883ad76e4b74d2f621d2
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-08-20 12:15:07 -0400

    Add extreme and input test cases

--- stack/stack.rb
+++ stack/stack.rb
@@ -9,9 +9,14 @@ class Stack
     @elements.size
   end
   def pop
+    raise Execption.new('Cannot pop an empty stack') if self.empty?
     @elements.pop
   end
   def top
+    raise Execption.new('Cannont pick on an empty stack') if self.empty?
     @elements.last
+  end
+  def empty?
+    @elements.empty?
   end
 end
--- stack/test_stack.rb
+++ stack/test_stack.rb
@@ -1,6 +1,49 @@
 require 'minitest/autorun'
 require_relative 'stack'
 class TestStack < MiniTest::Test
+  def test_stack_should_be_empty_on_construction
+    stack = Stack.new
+    assert stack.empty?, "Stack should be empty"
+  end
+
+  def test_after_n_pushes_to_an_empty_stack_the_stack_is_not_empty
+    stack = Stack.new
+    stack.push(2)
+    stack.push(4)
+    refute stack.empty?, "Stack is not empty"
+  end
+
+  def test_after_n_pushes_to_an_empty_stack_its_size_is_n
+    stack = Stack.new
+    stack.push(2)
+    stack.push(4)
+    assert_equal 2, stack.size, "Stack have 2 elements"
+
+  end
+  
+  def test_stack_with_n_elements_becomes_empty_after_n_pops
+    stack = Stack.new
+    stack.push(2)
+    stack.push(4)
+    stack.pop
+    stack.pop
+    assert_equal 0, stack.size, "Stack have 0 elements"
+    assert stack.empty?, "Stack is empty"
+  end
+
+  def test_popping_from_an_empty_stack_throws_an_execption
+    stack = Stack.new
+    assert_raises do
+      stack.pop
+    end
+  end
+
+  def test_picking_form_an_empty_stack_throws_an_exception
+    stack = Stack.new
+    assert_raises do
+      stack.top
+    end
+  end
   def test_sould_push_a_given_item
     stack = Stack.new
     stack.push(2)
